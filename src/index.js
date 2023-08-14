2d1e3199f4b0c20c4434aed1441d0695e89228d9
anusha5587 <anoosha.anand.24@gmail.com>
2023-08-14 20:19:53 +0100

    Added Celsius and Fahrenheit converter events in JS

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ function displayTemperature(response) {
   let mintempElement = document.querySelector("#mintemperature");
   let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   console.log(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -71,8 +73,30 @@ function weatherDataByGeolocation() {
   }
 }
 
-search("London");
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#tempNumber");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displaycelsiusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#tempNumber");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+
+let celsiusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchingCity);
 let currentlocation = document.querySelector("#current-location-btn");
 currentlocation.addEventListener("click", weatherDataByGeolocation);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+fahrenheitLink.addEventListener("click", displaycelsiusTemp);
+
+search("London");
